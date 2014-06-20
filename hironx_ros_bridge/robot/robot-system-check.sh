ddd43471fa5b3fbd816177c22172be4450c555a5
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2014-06-20 13:11:35 +0900

    add all option

--- hironx_ros_bridge/robot/robot-system-check.sh
+++ hironx_ros_bridge/robot/robot-system-check.sh
@@ -3,20 +3,26 @@
 function usage {
     echo >&2 "usage: $0 [hostname (default:hiro014)] [username (default:hiro)]"
     echo >&2 "          [-h|--help] print this message"
+    echo >&2 "          [-a|--all]  check system and jsk code"
     exit 0
 }
 
 # command line parse
-OPT=`getopt -o h -l help -- $*`
+OPT=`getopt -o ha -l help,all -- $*`
 if [ $? != 0 ]; then
     usage
 fi
 
 eval set -- $OPT
 
+# system check : check qnx and pre-installed software
+# jsk check (check_all) : check code in start-jsk (openrtm,openhrp3,hrpsys)
+CHECK_ALL=false
+
 while [ -n "$1" ] ; do
     case $1 in
         -h|--help) usage ;;
+        -a|--all) CHECK_ALL=true; shift 2; break ;;
         --) shift; break;;
         *) echo "Unknown option($1)"; usage;;
     esac
@@ -38,7 +44,8 @@ hostname=${hostname:="hiro014"}
 userid=$2
 userid=${userid:="hiro"} 
 
-echo ";; Copying check script to $userid@$hostname"
+echo ";; Copying check script to $userid@$hostname, SYSTEM_CODE_CHECK=true, JSK_CODE_CHECK=$CHECK_ALL"
+exit
 scp -r ./check $userid@$hostname:/tmp
 echo ";; Execute check scripts"
 ssh $userid@$hostname -t $commands 2>&1 | tee robot-system-check-$hostname.log
